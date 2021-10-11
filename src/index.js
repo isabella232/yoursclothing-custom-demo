8c8917ad75d1fc45818a120d31cef2e4a8a1b8ba
Alexandre Viretti <alexou73@gmail.com>
2021-10-11 14:50:26 +0200

    facets ok

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 //REACT REDUX CONFIF
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/indexReducer';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/indexReducer";
+import { Provider } from "react-redux";
 
 // DEV TOOL
 const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //CREATE STORE +IMPORT ALL REDUCER IN ONE
 const store = createStore(
-    rootReducer,
-    composeEnchancer(applyMiddleware(thunk))
+  rootReducer,
+  composeEnchancer(applyMiddleware(thunk))
 );
 
 //CHANGE ME IF USING NEW INDEX
-window.appID = 'RSBCBF0EG8';
-window.key = '05fcc691f3a833263d63a9b425e14176';
-window.index = 'yoursclothing_demo';
+window.appID = "RSBCBF0EG8";
+window.key = "05fcc691f3a833263d63a9b425e14176";
+window.index = "yoursclothing_demo";
 
 //SORTBY
-window.index_asc = 'yoursclothing_demo_price_asc';
-window.index_desc = 'yoursclothing_demo_price_desc';
+window.index_asc = "yoursclothing_demo_price_asc";
+window.index_desc = "yoursclothing_demo_price_desc";
 
 // QUERY SUGG
-window.indexSugg = 'yoursclothing_demo_query_suggestions';
+window.indexSugg = "yoursclothing_demo_query_suggestions";
 
 // DESCRIPTION ABOUT HOW THE FOLLOWING VARIABLES ARE USED
 // Please ensure you set your attributes in the following parts of the dashboard first:
@@ -36,17 +36,17 @@ window.indexSugg = 'yoursclothing_demo_query_suggestions';
 // If you don't see facet display, ask the internal dashboard team to make sure you have the favet ordering feature enabled
 
 // ADD ATTRIBUTES FOR REFINEMENT LISTS TO THE ARRAY, IF NONE LEAVE ARRAY EMPTY
-window.refinementListAttributes = [''];
+window.refinementListAttributes = ['availability','color','condition', 'material', 'size'];
 
 // ADD ATTRIBUTE FOR PRICE, IF NONE LEAVE AS EMPTY STRING
-window.priceAttribute = '';
+window.priceAttribute = "";
 
 // ADD ATTRIBUTE FOR HIERARCHY, IF NONE LEAVE AS EMPTY STRING
-window.hierarchicalCategoriesAttribute = '';
+window.hierarchicalCategoriesAttribute = "categories";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
