6981353123aa0eaca427fb3024cb20875493b27e
Matthew Foyle <mfoyle93@gmail.com>
2021-10-12 13:28:36 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 //REACT REDUX CONFIF
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/indexReducer";
-import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/indexReducer'
+import { Provider } from 'react-redux'
 
 // DEV TOOL
-const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 //CREATE STORE +IMPORT ALL REDUCER IN ONE
-const store = createStore(
-  rootReducer,
-  composeEnchancer(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, composeEnchancer(applyMiddleware(thunk)))
 
 //CHANGE ME IF USING NEW INDEX
-window.appID = "RSBCBF0EG8";
-window.key = "05fcc691f3a833263d63a9b425e14176";
-window.index = "yoursclothing_demo_price";
+window.appID = 'RSBCBF0EG8'
+window.key = '05fcc691f3a833263d63a9b425e14176'
+window.index = 'yoursclothing_demo_cats3'
 
 //SORTBY
-window.index_asc = "yoursclothing_demo_price_price_asc";
-window.index_desc = "yoursclothing_demo_price_price_desc";
+window.index_asc = 'yoursclothing_demo_price_price_asc'
+window.index_desc = 'yoursclothing_demo_price_price_desc'
 
 // QUERY SUGG
-window.indexSugg = "yoursclothing_demo_query_suggestions";
+window.indexSugg = 'yoursclothing_demo_query_suggestions'
 
 // DESCRIPTION ABOUT HOW THE FOLLOWING VARIABLES ARE USED
 // Please ensure you set your attributes in the following parts of the dashboard first:
@@ -37,27 +34,27 @@ window.indexSugg = "yoursclothing_demo_query_suggestions";
 
 // ADD ATTRIBUTES FOR REFINEMENT LISTS TO THE ARRAY, IF NONE LEAVE ARRAY EMPTY
 window.refinementListAttributes = [
-  "availability",
-  "color",
-  "condition",
-  "material",
-  "size",
-  "brand"
-];
+  'availability',
+  'color',
+  'condition',
+  'material',
+  'size',
+  'brand',
+]
 
 // ADD ATTRIBUTE FOR PRICE, IF NONE LEAVE AS EMPTY STRING
-window.priceAttribute = "price";
+window.priceAttribute = 'price'
 
 // ADD ATTRIBUTE FOR HIERARCHY, IF NONE LEAVE AS EMPTY STRING
 window.hierarchicalCategoriesAttribute = [
   'categories.lvl0',
   'categories.lvl1',
   'categories.lvl2',
-];
+]
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
