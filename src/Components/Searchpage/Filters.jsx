e4a84b0aa42ed82e27c362bc548fc1a538608c2f
Alexandre Viretti <alexou73@gmail.com>
2021-10-13 09:30:03 +0200

    resolve

--- src/Components/Searchpage/Filters.jsx
+++ src/Components/Searchpage/Filters.jsx
@@ -210,7 +210,7 @@ const CustomFilters = ({ filterAnim }) => {
         // fallbackComponent={Menu}
         >
           <HierarchicalMenu
-            attribute={window.hierarchicalCategoriesAttribute} title={window.hierarchicalCategoriesAttribute}
+            attributes={window.hierarchicalCategoriesAttribute} title={window.hierarchicalCategoriesAttribute}
           />
           {window.refinementListAttributes.map((e) => (
             <GenericRefinementList attribute={e} title={e} searchable />
